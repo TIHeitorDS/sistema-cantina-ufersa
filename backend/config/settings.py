7422baef41b0745b0f12b32bbdd61ee5da8b81e7
abner <abnergama2512@gmail.com>
2025-07-01 13:33:39 -0300

    ft: criar model cardapio, clientes, pedidos

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'cardapio',
+    'pedidos',
+    'clientes',
 ]
 
 MIDDLEWARE = [
---
+++ backend/pedidos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from ..clientes.models import Cliente
+
+# Create your models here.
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+    finalizado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Pedido #{self.id} - {self.cliente.nome}"
