dd0ff8a7e5650e8dd56f753de07910e35b9d7c41
TIHeitorDS <heitorclaudinods@gmail.com>
2025-07-03 22:20:02 -0300

    feat: add cardapio app URLs and views for API and HTML rendering

--- backend/cardapio/views.py
+++ backend/cardapio/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ProdutoSerializer
+from .models import Produto
 
-# Create your views here.
+# API REST
+class CardapioViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+# Página HTML
+def cardapio_list(request):
+    produtos = Produto.objects.all()
+    return render(request, 'cardapio/cardapio.html', {'produtos': produtos})
