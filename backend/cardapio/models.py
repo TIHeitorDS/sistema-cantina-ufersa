4ab3c5cfe11a7e97c4e1ff99d3273feb775ae69d
vinicius-a-almeida <vinicius45anacleto@gmail.com>
2025-07-05 12:54:10 -0300

    feat: images

--- backend/cardapio/models.py
+++ backend/cardapio/models.py
@@ -5,6 +5,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(decimal_places=2, max_digits=6)
     disponivel = models.BooleanField(default=True)
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
     
     def __str__(self):
         return self.nome
