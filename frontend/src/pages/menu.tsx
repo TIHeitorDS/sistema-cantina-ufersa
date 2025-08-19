28e7f525baaab09e11da73a58cf661e8f5e6d042
TIHeitorDS <heitorclaudinods@gmail.com>
2025-08-19 10:46:14 -0300

    feat: add success popup for cart actions; integrate with cart context and update menu page

--- frontend/src/pages/menu.tsx
+++ frontend/src/pages/menu.tsx
@@ -1,25 +1,34 @@
+import { useCart } from "../shared/hooks/useCart";
 import { items } from "../utils/data";
 import ItemCard from "../components/item-card";
 import AppLayout from "../ui/app-layout";
-import { useCart } from "../shared/hooks/useCart";
+import SucessPopup from "../ui/sucess-popup";
 
 export default function Menu() {
-  const { addItemToCart } = useCart();
+  const { addItemToCart, showPopup } = useCart();
 
   return (
-    <AppLayout title="Menu">
-      <div className="mt-6 grid grid-cols-2 lg:grid-cols-5 mx-auto gap-4">
-        {items.map(
-          (item) =>
-            item.disponivel && (
-              <ItemCard
-                key={item.id}
-                item={item}
-                onHandleCart={addItemToCart}
-              />
-            )
-        )}
+    <>
+      <AppLayout title="Menu">
+        <div className="mx-auto mt-6 grid grid-cols-2 gap-4 lg:grid-cols-5">
+          {items.map(
+            (item) =>
+              item.disponivel && (
+                <ItemCard
+                  key={item.id}
+                  item={item}
+                  onHandleCart={addItemToCart}
+                />
+              ),
+          )}
+        </div>
+      </AppLayout>
+
+      <div className="absolute bottom-24 left-1/2 w-4/5 -translate-x-1/2 space-y-3">
+        {showPopup.map((popup) => (
+          <SucessPopup key={popup.id} text={popup.text} />
+        ))}
       </div>
-    </AppLayout>
+    </>
   );
 }
