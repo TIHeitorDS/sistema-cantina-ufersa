3dd75ae7ac7e1748ff8ec76b1dd2eb39fee0da57
TIHeitorDS <heitorclaudinods@gmail.com>
2025-09-02 13:56:03 -0300

    feat: add Dockerfile and update requirements for gunicorn

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,8 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://localhost:5173"
 ]
 
 ROOT_URLCONF = 'config.urls'
